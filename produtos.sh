a67272f849e3f61d9182ee615270c8960e89a85a
josepaulo1233 <josehpaulo2124@gmail.com>
2025-08-29 12:53:30 -0300

    fix: add missing volume mount for ECMWF data in produtos.sh

--- produtos.sh
+++ produtos.sh
@@ -3,6 +3,7 @@ cmd="docker run --rm \
   -v /projetos/produtos-meteorologia/tmp:/app/tmp \
   -v /home/admin/.env:/root/.env \
   -v /WX2TB/Documentos:/WX2TB/Documentos \
+  -v /ftp/files_sftp/ECMWF:/ftp/files_sftp/ECMWF \
   -v /projetos/arquivos/meteorologia:/projetos/arquivos/meteorologia \
   -v /usr/local/grads-2.0.2.oga.2:/usr/local/grads-2.0.2.oga.2 \
   -e modelo=${modelo} \
