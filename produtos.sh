e4cd98ece4f833a3ef27ad4dc15cd957ce14cefb
josepaulo1233 <josehpaulo2124@gmail.com>
2025-08-29 12:10:41 -0300

    fix: remove unused variable assignments and streamline docker command in produtos.sh

--- produtos.sh
+++ produtos.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-modelo=$1
-data=$2
-hora=$3
-resolucao=$4
-sfcprefix=$5
-plprefix=$6
-shift 6  # remove os 6 primeiros argumentos fixos
-produtos="$@"  # tudo que sobrar vira lista de produtos
-
 cmd="docker run --rm \
   -v /projetos/produtos-meteorologia:/app \
   -v /projetos/produtos-meteorologia/tmp:/app/tmp \
@@ -16,17 +5,16 @@ cmd="docker run --rm \
   -v /WX2TB/Documentos:/WX2TB/Documentos \
   -v /projetos/arquivos/meteorologia:/projetos/arquivos/meteorologia \
   -v /usr/local/grads-2.0.2.oga.2:/usr/local/grads-2.0.2.oga.2 \
-  -e modelo=$modelo \
-  -e data=$data \
-  -e hora=$hora \
-  -e resolucao=$resolucao" \
-  --entrypoint tail \
-  produtos -f /dev/null"
+  -e modelo=${modelo} \
+  -e data=${data} \
+  -e hora=${hora} \
+  -e resolucao=${resolucao}"
 
-[ -n "$sfcprefix" ] && cmd="$cmd -e sfcprefix=$sfcprefix"
-[ -n "$plprefix" ] && cmd="$cmd -e plprefix=$plprefix"
-[ -n "$produtos" ] && cmd="$cmd -e produtos=\"$produtos\""
+[ -n "$sfcprefix" ] && cmd="$cmd -e sfcprefix=${sfcprefix}"
+[ -n "$plprefix" ] && cmd="$cmd -e plprefix=${plprefix}"
+[ -n "$produtos" ] && cmd="$cmd -e produtos='${produtos}'"
 
-cmd="$cmd produtos"
+# adiciona entrypoint e finaliza
+cmd="$cmd --entrypoint tail produtos -f /dev/null"
 
 eval $cmd
