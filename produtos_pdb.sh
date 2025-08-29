88a16cf877bc49de7830e6c18c74a825bdbed16b
josepaulo1233 <josehpaulo2124@gmail.com>
2025-08-29 10:43:23 -0300

    fix: simplify docker run command in produtos_pdb script by removing unused variables

--- produtos_pdb.sh
+++ produtos_pdb.sh
@@ -1,31 +1,11 @@
-#!/bin/bash
-
-modelo=$1
-data=$2
-hora=$3
-resolucao=$4
-sfcprefix=$5
-plprefix=$6
-shift 6  # remove os 6 primeiros argumentos fixos
-produtos="$@"  # tudo que sobrar vira lista de produtos
-
-cmd="docker run --rm \
+docker run --rm -dit \
   -v /projetos/produtos-meteorologia:/app \
   -v /projetos/produtos-meteorologia/tmp:/app/tmp \
   -v /home/admin/.env:/root/.env \
   -v /WX2TB/Documentos:/WX2TB/Documentos \
   -v /projetos/arquivos/meteorologia:/projetos/arquivos/meteorologia \
-  -v /usr/local/:/usr/local/ \
   -e modelo=$modelo \
   -e data=$data \
   -e hora=$hora \
-  -e resolucao=$resolucao" \
+  -e resolucao=$resolucao \
   ubuntu tail -f /dev/null
-
-[ -n "$sfcprefix" ] && cmd="$cmd -e sfcprefix=$sfcprefix"
-[ -n "$plprefix" ] && cmd="$cmd -e plprefix=$plprefix"
-[ -n "$produtos" ] && cmd="$cmd -e produtos=\"$produtos\""
-
-cmd="$cmd produtos"
-
-eval $cmd
