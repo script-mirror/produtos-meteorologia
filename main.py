7d7867da667403af0982b5257c9ae7936a6b2b2d
josepaulo1233 <jose.Flores@raizen.com>
2025-11-17 15:35:01 -0300

    fix: add support for 'ecmwf' model in file removal logic

--- main.py
+++ main.py
@@ -940,7 +940,7 @@ def main():
                         func()
 
         # Remove arquivos
-        if args.modelo_fmt not in ['cfsv2', 'cfsv2-mensal', 'nmme', 'c3s']:
+        if args.modelo_fmt not in ['cfsv2', 'cfsv2-mensal', 'nmme', 'c3s', 'ecmwf']:
            produtos.remove_files()
 
     else:
